13de013d98a043f4eb901410cc72aae9ecfbdd7a
AXeL-dev <contact.axel.dev@gmail.com>
2019-10-27 14:29:21 +0100

    Update outlook-unread-count.js
    based on this commit: https://github.com/ramboxapp/community-edition/commit/41977ea554c57992c137ce1d963a8861705369cb

--- outlook-unread-count.js
+++ outlook-unread-count.js
@@ -1,12 +1,8 @@
 function checkUnread() {
-    var elem = document.querySelector('i[data-icon-name="Inbox"]+span+span>span');
-    var count = elem ? elem.innerText.split('\n')[0] : 0;
-    var t = parseInt(count);
-    updateBadge(t);
+  var e = null !== document.querySelector(".ms-FocusZone i[data-icon-name=Inbox]").parentNode.querySelector("span span") ? parseInt(document.querySelector(".ms-FocusZone i[data-icon-name=Inbox]").parentNode.querySelector("span span").innerText) : 0;
+  updateBadge("" === e ? 0 : parseInt(e));
 }
-
 function updateBadge(e) {
-    e && e >= 1 ? rambox.setUnreadCount(e) : rambox.clearUnreadCount();
+  1 <= e ? rambox.setUnreadCount(e) : rambox.clearUnreadCount();
 }
-
 setInterval(checkUnread, 3000);
